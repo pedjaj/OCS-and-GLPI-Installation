3505df1af0fcc7d0b801e6c775bd143ad6938d49
Muhamad Faiz <east_pack@yahoo.com>
2017-08-24 15:23:47 +0800

    no message

--- ocssetup.sh
+++ ocssetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 setenforce 0
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf && echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
 yum -y update
@@ -13,9 +14,9 @@ yum -y install epel-release rpm-build redhat-rpm-config
 yum -y install mysql-community-release-el7-5.noarch.rpm proj
 yum -y install tinyxml libzip mysql-workbench-community
 yum -y install unzip
-yum install -y mod_perl perl-Archive-Zip perl-CPAN perl-YAML
-yum install -y perl-Digest-SHA1 php-pecl-zip php-gd php5-gd perl-XML-Simple perl-Net-IP perl-SOAP-Lite httpd-devel perl-DBI perl-DBD-MySQL perl-Compress-Zlib perl-Apache-DBI
-yum install -y --enablerepo=epel perl-Apache-DBI perl-XML-Entities
+yum -y install mod_perl perl-Archive-Zip perl-CPAN perl-YAML
+yum -y install perl-Digest-SHA1 php-pecl-zip php-gd php5-gd perl-XML-Simple perl-Net-IP perl-SOAP-Lite httpd-devel perl-DBI perl-DBD-MySQL perl-Compress-Zlib perl-Apache-DBI
+yum -y install --enablerepo=epel perl-Apache-DBI perl-XML-Entities
 chmod 755 -R /var/www/;
 printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
 /usr/bin/mysql_secure_installation
